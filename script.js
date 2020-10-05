60dd8f4d8cabda08ca9b7219a2ca831d1fa5945b
JonathanDegrv <dejo2803@student.iepscf-uccle.be>
2020-10-05 22:38:28 +0200

    Tp04

---
+++ script.js
@@ -0,0 +1,26 @@
+//recherche minimum
+function minimum(a,b){
+    if(a === b || a < b)
+    {
+        return a;
+    }
+    else
+    {
+        return b;
+    }
+}
+
+
+//recherche maximum
+function maximum(a,b){
+    if(a === b || a > b)
+    {
+        return a;
+    }
+    else
+    {
+        return b;
+    }
+}
+
+
